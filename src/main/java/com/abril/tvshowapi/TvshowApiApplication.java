793f7279020a104575a234629244ab9a07770cad
abril <13170789@itculiacan.edu.mx>
2019-07-14 19:10:13 -0600

    uploading project

---
+++ src/main/java/com/abril/tvshowapi/TvshowApiApplication.java
@@ -0,0 +1,13 @@
+package com.abril.tvshowapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvshowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvshowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abril/tvshowapi/TvshowApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abril.tvshowapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TvshowApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
